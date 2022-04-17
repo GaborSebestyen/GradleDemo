7c20909f4f9d257cfcefe71371bf2c9ad6359dfe
GaborSebestyen <96072956+GaborSebestyen@users.noreply.github.com>
2022-04-17 19:56:12 +0100

    Create AppTest.java

---
+++ app/src/test/java/GradleDemo/AppTest.java
@@ -0,0 +1,22 @@
+package GradleDemo;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class AppTest{
+    @Test public void appHasAGreeting(){
+        //Arrange
+        App myApp = new App();
+        //Act and Assert
+        assertNotNull(myApp.getGreeting(), "app should have a greeting");
+    }
+
+    @Test public void appFailingTest(){
+        //Arrange
+        App myApp = new App();
+        //Act and Assert
+        assertNull(myApp.getGreeting(), "test will fail");
+    }
+}
